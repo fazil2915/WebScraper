1865abdbe55a9de9003fe71a0082bee1109451cc
fazil rahman <fazil2915@gmail.com>
2022-10-25 13:15:47 +0530

    ejs added

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express=require("express")
 const jsdom =require("jsdom")
 const {JSDOM}=jsdom;
 const axios=require("axios");
-const { response } = require("express");
 const app=express()
+app.set('view engine','ejs');
 
 const URL="https://www.amazon.in/s?k=iphone14%2Bpro&crid=4NFENRG65RU2&sprefix=iphone1%2Caps%2C297&ref=nb_sb_ss_deep-retrain-50-ops-acceptance_2_7"
 async function fetchData(){
@@ -14,17 +14,31 @@ async function fetchData(){
         })
         const {document} =(new JSDOM(response.data)).window
 
-        console.log(document.querySelector(".s-card-container .s-image").getAttribute("src"));
-        console.log(document.querySelector(".s-card-container h2 span ").textContent);
-        console.log(document.querySelector(".s-card-container .a-price-whole").textContent);
+        const products=[]
+
+        document.querySelectorAll(".s-card-container").forEach(element=>{
+            products.push({
+                image:element.querySelector(".s-image").src,
+                title:element.querySelector("h2 span").textContent,
+                price:element.querySelector(".a-price-whole").textContent
+            })
+        })
+          //console.log(products);
+          return products
+        // console.log(document.querySelector(".s-card-container .s-image").getAttribute("src"));
+        // console.log(document.querySelector(".s-card-container h2 span ").textContent);
+        // console.log(document.querySelector(".s-card-container .a-price-whole").textContent);
 
     }catch(error){
         console.log(error);
     }
 
 }
+app.get('/',async(req,res)=>{
+    const products=await fetchData()
+res.render("pages/index.ejs",{products});
+})
 
-fetchData()
 
 //s-card-container s-image
 //a-price
