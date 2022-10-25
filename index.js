0130e6138d575f5e9539a6eeb2f844d78124dd4c
fazil rahman <fazil2915@gmail.com>
2022-10-25 14:14:42 +0530

    view structured

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const axios=require("axios");
 const app=express()
 app.set('view engine','ejs');
 
-const URL="https://www.amazon.in/s?k=iphone14%2Bpro&crid=4NFENRG65RU2&sprefix=iphone1%2Caps%2C297&ref=nb_sb_ss_deep-retrain-50-ops-acceptance_2_7"
+
+const URL="https://www.amazon.in/s?k=apple+iphone14pro+max&crid=207AKBM75PL7C&sprefix=iphone14pro%2Caps%2C721&ref=nb_sb_ss_pltr-ranker-engagementacceptance_1_11"
+
 async function fetchData(){
     try{
         const response= await axios.get(URL,{
@@ -25,15 +27,14 @@ async function fetchData(){
         })
           //console.log(products);
           return products
-        // console.log(document.querySelector(".s-card-container .s-image").getAttribute("src"));
-        // console.log(document.querySelector(".s-card-container h2 span ").textContent);
-        // console.log(document.querySelector(".s-card-container .a-price-whole").textContent);
+        
 
     }catch(error){
         console.log(error);
     }
 
 }
+
 app.get('/',async(req,res)=>{
     const products=await fetchData()
 res.render("pages/index.ejs",{products});
